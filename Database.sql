b1af26df4633ab585ba2e7b8dbee3e229798a9b7
buicongdanh <64085482+buicongdanh@users.noreply.github.com>
2021-11-21 15:53:54 +0700

    Update Database.sql
    Sửa lại toàn bộ dữ liệu

--- Database.sql
+++ Database.sql
@@ -1,21 +1,32 @@
 ﻿CREATE DATABASE QLHH  --quản lí hàng hóa
+USE QLHH
+--drop database QLHH
 
--- drop database QLHH
+
+CREATE TABLE TAIKHOAN
+(
+	TENNGUOIDUNG	CHAR(13),
+	MATKHAU			CHAR(13),
+	LOAITAIKHOAN	NVARCHAR(10),
+	KICHHOAT		BIT,
+
+	CONSTRAINT PK_TAIKHOAN
+	PRIMARY KEY (TENNGUOIDUNG)
+)
+
 
 CREATE TABLE DOITAC
 (
-	MADT Char(10),
-	HOTEN Ntext,
-	NGUOIDAIDIEN NText,
-	THANHPHO NVarchar(30),
-	QUAN NVarchar(20),
-	SOCHINHANH Int,
-	SLDHMOINGAY Int,
-	LOAIHANG NVarchar(50),
-	DIACHI NText,
-	SODTH Char(10),
-	EMAIL Varchar(50),
-
+	MADT			Char(10),
+	NGUOIDAIDIEN	NText,
+	THANHPHO		NVarchar(30),
+	QUAN			NVarchar(20),
+	LOAIHANG		NVarchar(50),
+	DIACHI			NVarchar(50),
+	HOTEN			NTEXT,
+	SODTH			CHAR(10),
+	EMAIL			Varchar(50),
+	TENNGUOIDUNG	CHAR(13),
 	CONSTRAINT PK_DOITAC
 	PRIMARY KEY (MADT)
 )
@@ -31,23 +42,25 @@ CREATE TABLE DOITAC
 
 CREATE TABLE HOPDONG
 (
-	MAHD	Char(10),
-	MASOTHUEDOITAC	Char(10),
-	SOCHINHANHDANGKY SmallInt,
-	PHIKICHHOAT	Money,
-	PHIHOAHONG	Money,
+	MAHD			Char(10),
+	MASOTHUE		Char(10),
+	NGUOIDAIDIEN	SmallInt,
+	PTHOAHONG		Money,
 	THOIGIANHIEULUC	Date,
-	MADT CHAR(10),
+	MADT			CHAR(10),
+
 	CONSTRAINT PK_HOPDONG
 	PRIMARY KEY (MAHD)
 )
 
 CREATE TABLE KHACHHANG
 (
-	MAKH Char(10),
-	HOTEN Ntext,
-	SODTH	Char(10),
-	EMAIL	Varchar(50),
+	MAKH			Char(10),
+	HOTEN			Ntext,
+	DIACHI			NVARCHAR(50),
+	SODTH			Char(10),
+	EMAIL			Varchar(50),
+	TENNGUOIDUNG	CHAR(13),
 
 	CONSTRAINT PK_KHACHHANG
 	PRIMARY KEY (MAKH)
@@ -55,10 +68,11 @@ CREATE TABLE KHACHHANG
 
 CREATE TABLE SANPHAM
 (
-	MASP Char(10),
-	TENSANPHAM Ntext,	
-	GIA Money,
-	MADT CHAR(10),
+	MASP			Char(10),
+	TENSANPHAM		Ntext,	
+	GIA				Money,
+	TINHTRANG		BIT,
+	LOAIHANG		NVARCHAR,
 
 	CONSTRAINT PK_SANPHAM
 	PRIMARY KEY (MASP)	
@@ -66,14 +80,14 @@ CREATE TABLE SANPHAM
 
 CREATE TABLE DONHANG
 ( 
-	MADH	Char(10),
-	HINHTHUCTHANHTOAN	NVarchar(30),
+	MADH			Char(10),
+	HINHTHUCTT		NVarchar(30),
 	DIACHIGIAOHANG	NText,
-	PHISANPHAM	Money,
+	PHISANPHAM		Money,
 	PHIVANCHUYEN	Money,
-	TINHTRANG	NVarchar(20),
-	MATX Char(10),
-	MAKH CHAR(10),
+	TINHTRANG		NVarchar(20),
+	MATX			Char(10),
+	MAKH			CHAR(10),
 
 	CONSTRAINT PK_DONHANG
 	PRIMARY KEY (MADH)
@@ -81,9 +95,9 @@ CREATE TABLE DONHANG
 
 CREATE TABLE CHINHANH
 (
-	MADT CHAR(10),
-	DIACHI NTEXT,
-	STT INT
+	MADT			CHAR(10),
+	STT				INT,
+	DIACHI			NTEXT,
 
 	CONSTRAINT PK_CHINHANH
 	PRIMARY KEY (MADT,STT)
@@ -91,16 +105,16 @@ CREATE TABLE CHINHANH
 
 CREATE TABLE TAIXE
 (
-	MATX	Char(10),
-	HOTEN	NText,
-	CMND	Char(12),
-	SODTH	Char(10),
-	DIACHI	NText,
-	BIENSO	Varchar(12),
-	KHUVUC	NText,
-	EMAIL	Varchar(50),
-	PhiTheChan Money,
-	TKNGANHANG	Varchar(20),
+	MATX			Char(10),
+	HOTEN			NText,
+	CMND			Char(12),
+	SODTH			Char(10),
+	DIACHI			NText,
+	BIENSO			Varchar(12),
+	KHUVUC			NText,
+	EMAIL			Varchar(50),
+	TKNGANHANG		Varchar(20),
+	TENNGUOIDUNG	CHAR(13),
 
 	CONSTRAINT PK_TAIXE
 	PRIMARY KEY (MATX)
@@ -108,33 +122,84 @@ CREATE TABLE TAIXE
 
 CREATE TABLE CT_DONHANG
 (
-	MADH CHAR(10), 
-	MASP CHAR(10), 
-	SOLUONG INT,
+	MADH			CHAR(10), 
+	MASP			CHAR(10), 
+	SOLUONG			INT,
 
 	CONSTRAINT PK_CT_DONHANG
 	PRIMARY KEY (MADH,MASP)
 )
 
+CREATE TABLE DS_SANPHAM
+(
+	MASP			CHAR(10),
+	MADT			CHAR(10),
+	STT				INT,
+	SOLUONG			INT,
 
+	CONSTRAINT PK_DS_SANPHAM
+	PRIMARY KEY (MADT,STT,MASP)
+)
+
+CREATE TABLE NHANVIEN
+(
+	MANV			CHAR(10),
+	HOTEN			Ntext,
+	DIACHI			NVARCHAR(50),
+	SODTH			Char(10),
+	EMAIL			Varchar(50),
+	TENNGUOIDUNG	CHAR(13),
+
+	CONSTRAINT PK_NHANVIEN
+	PRIMARY KEY (MANV)
+)
 -- Tạo khóa ngoại
-alter table SANPHAM add
-	constraint FK_SANPHAM_DOITAC foreign key (MADT) references DOITAC (MADT)
 
+--1) Doi tac - Hop dong
 alter table HOPDONG add
 	constraint FK_HOPDONG_DOITAC foreign key (MADT) references DOITAC (MADT)
 
+--2) Don hang - khach hang
 alter table DONHANG add
 	constraint FK_DONHANG_KHACHHANG foreign key (MAKH) references KHACHHANG (MAKH)
 
-alter table  DONHANG add
+--3) Don hang - Tai xe
+alter table DONHANG add
 	constraint FK_DONHANG_TAIXE foreign key (MATX) references TAIXE (MATX)
 
+--4) CT_Don hang - Don hang
 alter table CT_DONHANG add
 	constraint FK_CT_DONHANG_DONHANG foreign key (MADH) references DONHANG (MADH)
 
+--5) CT_Don hang - San pham
 alter table CT_DONHANG add
 	constraint FK_CT_DONHANG_SANPHAM foreign key (MASP) references SANPHAM (MASP)
 
+--6) Chi nhanh - Doi tac
 alter table CHINHANH add
 	constraint FK_CHINHANH_DOITAC foreign key (MADT) references DOITAC (MADT)
+
+--7) Chi nhanh - DS_San pham
+alter table DS_SANPHAM add
+	constraint FK_DS_SANPHAMCHINHANH foreign key (MADT, STT) references CHINHANH (MADT, STT)
+
+--8) San pham - DS_San pham
+alter table DS_SANPHAM add
+	constraint FK_SANPHAM_DS_SANPHAM foreign key (MASP) references SANPHAM (MASP)
+
+
+--9) Tai khoan - Doi tac
+alter table DOITAC add
+	constraint FK_DOITAC_TAIKHOAN foreign key (TENNGUOIDUNG) references TAIKHOAN (TENNGUOIDUNG)
+
+--10) Tai khoan - Khach hang
+alter table KHACHHANG add
+	constraint FK_KHACHHANG_TAIKHOAN foreign key (TENNGUOIDUNG) references TAIKHOAN (TENNGUOIDUNG)
+
+--11) Tai khoan - Tai xe
+alter table TAIXE add
+	constraint FK_TAIXE_TAIKHOAN foreign key (TENNGUOIDUNG) references TAIKHOAN (TENNGUOIDUNG)
+
+--12) Tai khoan - Nhan vien
+alter table NHANVIEN add
+	constraint FK_NHANVIEN_TAIKHOAN foreign key (TENNGUOIDUNG) references TAIKHOAN (TENNGUOIDUNG)
