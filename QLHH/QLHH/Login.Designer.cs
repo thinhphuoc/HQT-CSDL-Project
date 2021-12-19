270c505808241522d0fd5417120e2e89e01765e6
buicongdanh <64085482+buicongdanh@users.noreply.github.com>
2021-12-19 21:07:54 +0700

    UI update
    Cập nhật thanh Taskbar

---
+++ QLHH/QLHH/Login.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace QLHH
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_Menu = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.iconButton_CN5 = new FontAwesome.Sharp.IconButton();
+            this.iconButton_CN4 = new FontAwesome.Sharp.IconButton();
+            this.iconButton_CN3 = new FontAwesome.Sharp.IconButton();
+            this.iconButton_CN2 = new FontAwesome.Sharp.IconButton();
+            this.iconButton_CN1 = new FontAwesome.Sharp.IconButton();
+            this.panel_Menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel_Menu
+            // 
+            this.panel_Menu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.panel_Menu.Controls.Add(this.label1);
+            this.panel_Menu.Controls.Add(this.iconButton_CN5);
+            this.panel_Menu.Controls.Add(this.iconButton_CN4);
+            this.panel_Menu.Controls.Add(this.iconButton_CN3);
+            this.panel_Menu.Controls.Add(this.iconButton_CN2);
+            this.panel_Menu.Controls.Add(this.iconButton_CN1);
+            this.panel_Menu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_Menu.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.panel_Menu.Location = new System.Drawing.Point(0, 0);
+            this.panel_Menu.Name = "panel_Menu";
+            this.panel_Menu.Size = new System.Drawing.Size(1182, 753);
+            this.panel_Menu.TabIndex = 0;
+            this.panel_Menu.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Menu_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(543, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 54);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Đăng nhập";
+            // 
+            // iconButton_CN5
+            // 
+            this.iconButton_CN5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(40)))), ((int)(((byte)(90)))));
+            this.iconButton_CN5.FlatAppearance.BorderSize = 0;
+            this.iconButton_CN5.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.iconButton_CN5.ForeColor = System.Drawing.Color.White;
+            this.iconButton_CN5.IconChar = FontAwesome.Sharp.IconChar.Cogs;
+            this.iconButton_CN5.IconColor = System.Drawing.Color.Transparent;
+            this.iconButton_CN5.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton_CN5.IconSize = 32;
+            this.iconButton_CN5.Location = new System.Drawing.Point(403, 606);
+            this.iconButton_CN5.Name = "iconButton_CN5";
+            this.iconButton_CN5.Padding = new System.Windows.Forms.Padding(0, 0, 10, 0);
+            this.iconButton_CN5.Size = new System.Drawing.Size(468, 70);
+            this.iconButton_CN5.TabIndex = 5;
+            this.iconButton_CN5.Text = "Đăng nhập với tư cách là Đối Tác";
+            this.iconButton_CN5.UseVisualStyleBackColor = false;
+            this.iconButton_CN5.Click += new System.EventHandler(this.iconButton_CN5_Click);
+            // 
+            // iconButton_CN4
+            // 
+            this.iconButton_CN4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(40)))), ((int)(((byte)(90)))));
+            this.iconButton_CN4.FlatAppearance.BorderSize = 0;
+            this.iconButton_CN4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.iconButton_CN4.ForeColor = System.Drawing.Color.White;
+            this.iconButton_CN4.IconChar = FontAwesome.Sharp.IconChar.ListAlt;
+            this.iconButton_CN4.IconColor = System.Drawing.Color.Transparent;
+            this.iconButton_CN4.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton_CN4.IconSize = 32;
+            this.iconButton_CN4.Location = new System.Drawing.Point(403, 499);
+            this.iconButton_CN4.Name = "iconButton_CN4";
+            this.iconButton_CN4.Padding = new System.Windows.Forms.Padding(0, 0, 10, 0);
+            this.iconButton_CN4.Size = new System.Drawing.Size(468, 70);
+            this.iconButton_CN4.TabIndex = 4;
+            this.iconButton_CN4.Text = "Đăng nhập với tư cách là Khách Hàng";
+            this.iconButton_CN4.UseVisualStyleBackColor = false;
+            this.iconButton_CN4.Click += new System.EventHandler(this.iconButton_CN4_Click);
+            // 
+            // iconButton_CN3
+            // 
+            this.iconButton_CN3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(40)))), ((int)(((byte)(90)))));
+            this.iconButton_CN3.FlatAppearance.BorderSize = 0;
+            this.iconButton_CN3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.iconButton_CN3.ForeColor = System.Drawing.Color.White;
+            this.iconButton_CN3.IconChar = FontAwesome.Sharp.IconChar.Box;
+            this.iconButton_CN3.IconColor = System.Drawing.Color.Transparent;
+            this.iconButton_CN3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton_CN3.IconSize = 32;
+            this.iconButton_CN3.Location = new System.Drawing.Point(403, 396);
+            this.iconButton_CN3.Name = "iconButton_CN3";
+            this.iconButton_CN3.Padding = new System.Windows.Forms.Padding(0, 0, 10, 0);
+            this.iconButton_CN3.Size = new System.Drawing.Size(468, 70);
+            this.iconButton_CN3.TabIndex = 3;
+            this.iconButton_CN3.Text = "Đăng nhập với tư cách là Nhân Viên";
+            this.iconButton_CN3.UseVisualStyleBackColor = false;
+            this.iconButton_CN3.Click += new System.EventHandler(this.iconButton_CN3_Click);
+            // 
+            // iconButton_CN2
+            // 
+            this.iconButton_CN2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(40)))), ((int)(((byte)(90)))));
+            this.iconButton_CN2.FlatAppearance.BorderSize = 0;
+            this.iconButton_CN2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.iconButton_CN2.ForeColor = System.Drawing.Color.White;
+            this.iconButton_CN2.IconChar = FontAwesome.Sharp.IconChar.FileContract;
+            this.iconButton_CN2.IconColor = System.Drawing.Color.Transparent;
+            this.iconButton_CN2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton_CN2.IconSize = 32;
+            this.iconButton_CN2.Location = new System.Drawing.Point(403, 285);
+            this.iconButton_CN2.Name = "iconButton_CN2";
+            this.iconButton_CN2.Padding = new System.Windows.Forms.Padding(0, 0, 10, 0);
+            this.iconButton_CN2.Size = new System.Drawing.Size(468, 70);
+            this.iconButton_CN2.TabIndex = 2;
+            this.iconButton_CN2.Text = "Đăng nhập với tư cách là Tài Xế";
+            this.iconButton_CN2.UseVisualStyleBackColor = false;
+            this.iconButton_CN2.Click += new System.EventHandler(this.iconButton_CN2_Click);
+            // 
+            // iconButton_CN1
+            // 
+            this.iconButton_CN1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(40)))), ((int)(((byte)(90)))));
+            this.iconButton_CN1.FlatAppearance.BorderSize = 0;
+            this.iconButton_CN1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.iconButton_CN1.ForeColor = System.Drawing.Color.White;
+            this.iconButton_CN1.IconChar = FontAwesome.Sharp.IconChar.Screwdriver;
+            this.iconButton_CN1.IconColor = System.Drawing.Color.Transparent;
+            this.iconButton_CN1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton_CN1.IconSize = 32;
+            this.iconButton_CN1.Location = new System.Drawing.Point(403, 181);
+            this.iconButton_CN1.Name = "iconButton_CN1";
+            this.iconButton_CN1.Padding = new System.Windows.Forms.Padding(0, 0, 10, 0);
+            this.iconButton_CN1.Size = new System.Drawing.Size(468, 70);
+            this.iconButton_CN1.TabIndex = 1;
+            this.iconButton_CN1.Text = "Đăng nhập với tư cách là Admin";
+            this.iconButton_CN1.UseVisualStyleBackColor = false;
+            this.iconButton_CN1.Click += new System.EventHandler(this.iconButton_CN1_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 753);
+            this.Controls.Add(this.panel_Menu);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel_Menu.ResumeLayout(false);
+            this.panel_Menu.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_Menu;
+        private FontAwesome.Sharp.IconButton iconButton_CN5;
+        private FontAwesome.Sharp.IconButton iconButton_CN4;
+        private FontAwesome.Sharp.IconButton iconButton_CN3;
+        private FontAwesome.Sharp.IconButton iconButton_CN2;
+        private FontAwesome.Sharp.IconButton iconButton_CN1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
